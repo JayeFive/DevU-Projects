e92048f0b08ca187eced3d9f7cac78fa05b0d104
Johnny Olson <john.olson3@gmail.com>
2018-01-19 16:59:54 -0800

    Lesson 14 DateTime object

---
+++ CS-ASP Challenges/CS-ASP Challenges/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Working with date and time:<br />
+            <br />
+            <asp:Button ID="OkBtn" runat="server" OnClick="OkBtn_Click" Text="Ok" />
+            <br />
+            <br />
+            <asp:Label ID="ResultLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CS-ASP Challenges/CS-ASP Challenges/Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void OkBtn_Click(object sender, EventArgs e)
+    {
+        DateTime myValue = DateTime.Now;
+        // ResultLabel.Text = myValue.ToString();
+        // ResultLabel.Text = myValue.ToLongDateString();
+        // ResultLabel.Text = myValue.ToLongTimeString();
+        // ResultLabel.Text = myValue.ToShortDateString();
+        // ResultLabel.Text = myValue.ToShortTimeString();
+
+        // Do math to DateTime
+        // ResultLabel.Text = myValue.AddDays(2).ToString();
+        // ResultLabel.Text = myValue.AddMonths(-2).ToString();
+        // ResultLabel.Text = myValue.Month.ToString();
+        // ResultLabel.Text = myValue.DayOfYear.ToString();
+
+        // More ways to initialize DateTime
+        DateTime anotherDate = DateTime.Parse("4/3/1984");
+        // ResultLabel.Text = anotherDate.ToLongDateString();
+
+        DateTime moreDate = new DateTime(1984, 4, 3);
+        ResultLabel.Text = anotherDate.ToLongDateString();
+    }
+}
