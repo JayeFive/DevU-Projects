e13d9c87e665694141cfc0a21a789bf6c2793e2c
Johnny Olson <john.olson3@gmail.com>
2018-01-25 15:40:46 -0800

    Add inital random image functionality

--- CS-ASP Challenges/CS-ASP Challenges/Default.aspx.cs
+++ CS-ASP Challenges/CS-ASP Challenges/Default.aspx.cs
@@ -7,12 +7,20 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    Random random = new Random();
+
     protected void Page_Load(object sender, EventArgs e)
     {
-
-
+        ImageReel1.ImageUrl = imagePaths[random.Next(0, imagePaths.Length)];
+        ImageReel2.ImageUrl = imagePaths[random.Next(0, imagePaths.Length)];
+        ImageReel3.ImageUrl = imagePaths[random.Next(0, imagePaths.Length)];
 
     }
+
+    string[] imagePaths = new string[] 
+        { @".\images\bar.png", @".\images\Bell.png", @".\images\Cherry.png", @".\images\Clover.png",
+          @".\images\Diamond.png", @".\images\HorseShoe.png", @".\images\Lemon.png", @".\images\Orange.png",
+          @".\images\Plum.png", @".\images\Seven.png", @".\images\Strawberry.png", @".\images\Watermelon.png"};
 
     
     
