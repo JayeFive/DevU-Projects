9977a0389c3e48e51100b27df27298be57ff692f
Johnny Olson <john.olson3@gmail.com>
2018-01-25 12:42:21 -0800

    Refactor with helper method

--- CS-ASP Challenges/CS-ASP Challenges/Default.aspx
+++ CS-ASP Challenges/CS-ASP Challenges/Default.aspx
@@ -14,13 +14,13 @@
             <asp:TextBox ID="quantityTextBox" runat="server" AutoPostBack="True" OnTextChanged="quantityTextBox_TextChanged"></asp:TextBox>
             <br />
             <br />
-            <asp:RadioButton ID="fromCupsRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="cupsRadio_CheckedChanged" Text="Cups" />
+            <asp:RadioButton ID="fromCupsRadio" runat="server" AutoPostBack="True" GroupName="FromGroup" OnCheckedChanged="cupsRadio_CheckedChanged" Text="Cups" />
             <br />
-            <asp:RadioButton ID="fromPintsRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="fromPintsRadio_CheckedChanged" Text="Pints" />
+            <asp:RadioButton ID="fromPintsRadio" runat="server" AutoPostBack="True" GroupName="FromGroup" OnCheckedChanged="fromPintsRadio_CheckedChanged" Text="Pints" />
             <br />
-            <asp:RadioButton ID="fromQuartsRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="fromQuartsRadio_CheckedChanged" Text="Quarts" />
+            <asp:RadioButton ID="fromQuartsRadio" runat="server" AutoPostBack="True" GroupName="FromGroup" OnCheckedChanged="fromQuartsRadio_CheckedChanged" Text="Quarts" />
             <br />
-            <asp:RadioButton ID="fromGallonsRadio" runat="server" AutoPostBack="True" GroupName="fromGroup" OnCheckedChanged="fromGallonsRadio_CheckedChanged" Text="Gallons" />
+            <asp:RadioButton ID="fromGallonsRadio" runat="server" AutoPostBack="True" GroupName="FromGroup" OnCheckedChanged="fromGallonsRadio_CheckedChanged" Text="Gallons" />
             <br />
             <br />
             <asp:Label ID="resultLabel" runat="server"></asp:Label>
--- CS-ASP Challenges/CS-ASP Challenges/Default.aspx.cs
+++ CS-ASP Challenges/CS-ASP Challenges/Default.aspx.cs
@@ -13,57 +13,45 @@ public partial class _Default : System.Web.UI.Page
     }
     protected void cupsRadio_CheckedChanged(object sender, EventArgs e)
     {
-        if (quantityTextBox.Text.Trim().Length == 0)
-            return;
-
-        double quantity = 0.0;
-        if (!Double.TryParse(quantityTextBox.Text, out quantity))
-            return;
-
-        resultLabel.Text = "The number of cups: " + quantity.ToString();
+        CalculateCups();
     }
 
     protected void fromPintsRadio_CheckedChanged(object sender, EventArgs e)
     {
-        if (quantityTextBox.Text.Trim().Length == 0)
-            return;
-
-        double quantity = 0.0;
-        if (!Double.TryParse(quantityTextBox.Text, out quantity))
-            return;
-
-        double cups = quantity * 2.0;
-        resultLabel.Text = "The number of cups: " + cups.ToString();
+        CalculateCups();
     }
 
     protected void fromQuartsRadio_CheckedChanged(object sender, EventArgs e)
     {
-        if (quantityTextBox.Text.Trim().Length == 0)
-            return;
-
-        double quantity = 0.0;
-        if (!Double.TryParse(quantityTextBox.Text, out quantity))
-            return;
-
-        double cups = quantity * 4.0;
-        resultLabel.Text = "The number of cups: " + cups.ToString();
+        CalculateCups();
     }
 
     protected void fromGallonsRadio_CheckedChanged(object sender, EventArgs e)
     {
-        if (quantityTextBox.Text.Trim().Length == 0)
-            return;
-
-        double quantity = 0.0;
-        if (!Double.TryParse(quantityTextBox.Text, out quantity))
-            return;
-
-        double cups = quantity * 16.0;
-        resultLabel.Text = "The number of cups: " + cups.ToString();
+        CalculateCups();
     }
 
     protected void quantityTextBox_TextChanged(object sender, EventArgs e)
     {
         // ?  Right now, this doens't work!
+    }
+
+    private void CalculateCups()
+    {
+        if (quantityTextBox.Text.Trim().Length == 0)
+            return;
+
+        double quantity = 0.0;
+        if (!Double.TryParse(quantityTextBox.Text, out quantity))
+            return;
+
+        double cups = 0.0;
+
+        if (fromCupsRadio.Checked) cups = quantity;
+        else if (fromPintsRadio.Checked) cups = quantity * 2;
+        else if (fromQuartsRadio.Checked) cups = quantity * 4;
+        else if (fromGallonsRadio.Checked) cups = quantity * 16;
+
+        resultLabel.Text = "The number of cups: " + cups.ToString();
     }
 }
