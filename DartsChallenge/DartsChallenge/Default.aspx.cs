e001da6a69a6687f3670e9de9541682dc8efaecd
Johnny Olson <john.olson3@gmail.com>
2018-02-03 00:21:25 -0800

    Refactor for a main player switching loop

--- DartsChallenge/DartsChallenge/Default.aspx.cs
+++ DartsChallenge/DartsChallenge/Default.aspx.cs
@@ -3,8 +3,8 @@ using Darts;
 
 public partial class _Default : System.Web.UI.Page
 {
-    Player Player1 = new Player();
-    Player Player2 = new Player();
+    Player PlayerOne = new Player();
+    Player PlayerTwo = new Player();
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -13,29 +13,28 @@ public partial class _Default : System.Web.UI.Page
 
     protected void PlayDartsButton_Click(object sender, EventArgs e)
     {
-        Player[] activePlayer = new Player[] { Player1, Player2 };
-        StartGame();
+        bool PlayerOneActive = true;
+        MainLoop(PlayerOneActive);
     }
 
-    //void StartGame()
-    //{
-    //    if (activeTurnCounter != 1)
-    //    {
-    //        active
-    //    }
-    //}
+    private void MainLoop(bool PlayerOneActive)
+    {
+        if (PlayerOneActive)
+        {
+            PlayersTurn(PlayerOne);
+            PlayerOneActive = false;
+        }
+        else
+        {
+            PlayersTurn(PlayerTwo);
+            PlayerOneActive = true;
+        }
+        
+    }
 
     private void PlayersTurn(Player activePlayer)
     {
-        Dart dart = new Dart();
-        var throwScore = 0;
-        for (var i = 1; i < 3; i++)     // Each player get three throws
-        {
-            throwScore = dart.Throw();
-            if (throwScore + activePlayer.Score > 301) return;  // return omitting last throw score
-            else if (throwScore + activePlayer.Score == 301) return; // win condition
-            else activePlayer.Score += throwScore;
-        }
+
     }
 }
 
