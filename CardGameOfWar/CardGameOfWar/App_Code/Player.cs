822f36c8c4badb6d569b687146a28f902da98492
Johnny Olson <john.olson3@gmail.com>
2018-02-06 17:09:01 -0800

    Add PlayerHand card list

--- CardGameOfWar/CardGameOfWar/App_Code/Player.cs
+++ CardGameOfWar/CardGameOfWar/App_Code/Player.cs
@@ -1,4 +1,17 @@
-﻿class Player
+﻿using System.Collections.Generic;
+
+namespace GameOfWar
 {
+    class Player
+    {
+        public string Name { get; set; }
+        public List<Card> PlayerHand { get; set; }
+
+        public Player()
+        {
+            PlayerHand = new List<Card>();
+        }
+    }
+
 
 }
