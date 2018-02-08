8e39b00074daf3e0ffe53cd525d4838ae6a01152
Johnny Olson <john.olson3@gmail.com>
2018-02-08 13:51:18 -0800

    Change playerHand to Queue type collection (FIFO instead of LIFO)

--- CardGameOfWar/CardGameOfWar/App_Code/Player.cs
+++ CardGameOfWar/CardGameOfWar/App_Code/Player.cs
@@ -5,11 +5,11 @@ namespace GameOfWar
     public class Player
     {
         public string Name { get; set; }
-        public Stack<Card> PlayerHand { get; set; }
+        public Queue<Card> PlayerHand { get; set; }
 
         public Player()
         {
-            PlayerHand = new Stack<Card>();
+            PlayerHand = new Queue<Card>();
         }
     }
 
--- CardGameOfWar/CardGameOfWar/Default.aspx.cs
+++ CardGameOfWar/CardGameOfWar/Default.aspx.cs
@@ -8,6 +8,7 @@ public partial class _Default : System.Web.UI.Page
     Dealer dealer = new Dealer();
     Player playerOne = new Player();
     Player playerTwo = new Player();
+    const int numberOfRoundsToPlay = 10;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -17,6 +18,10 @@ public partial class _Default : System.Web.UI.Page
     protected void playButton_Click(object sender, EventArgs e)
     {
         dealCards();
+        for (int i = 0; i < numberOfRoundsToPlay; i++) 
+        {
+            BeginRound();
+        }
 
         resultLabel.Text += "<h2>Player One's Cards: </h2> <br />";
         foreach(var card in playerOne.PlayerHand)
@@ -35,5 +40,21 @@ public partial class _Default : System.Web.UI.Page
     {
         dealer.ShuffleCards(deck);
         dealer.DealEntireDeck(new Player[] { playerOne, playerTwo }, deck);
+    }
+
+    private void BeginRound()
+    {
+        // Each player draws top card from hand
+        var playerOneCard = playerOne.PlayerHand.Dequeue();
+        var PlayerTwoCard = playerTwo.PlayerHand.Dequeue();
+        // Evaluate winner
+        if (playerOneCard.CardNumber > PlayerTwoCard.CardNumber)
+        {
+
+        }
+        
+
+            // if tied, WAR
+            // else, winning card holder gets both cards which goto the bottom of the hand
     }
 }
